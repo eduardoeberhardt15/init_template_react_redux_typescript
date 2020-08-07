b0fce83215efdd3140684bcb5dc7d40eb37bcbd6
Eduardo Eberhardt <eduardoeberhardt@gmail.com>
2020-08-07 11:41:02 -0300

    add styles.css

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import {Provider} from 'react-redux';
 import store from './components/store';
 
+import Todo from './components/Todo';
+
+import "./global.css";
+
 function App() {
   return (
     <Provider store={store}>
-    <div className="App">
-      hello world
-    </div>
+    <Todo />
     </Provider>
   );
 }
